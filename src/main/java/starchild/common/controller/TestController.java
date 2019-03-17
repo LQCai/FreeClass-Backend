cd574a0ab9253aed833e157807d3fff1c26caddf
lqcai <1031868928@qq.com>
2019-03-17 16:14:35 +0800

    SSM初始化

---
+++ src/main/java/starchild/common/controller/TestController.java
@@ -0,0 +1,21 @@
+package starchild.common.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+
+    @RequestMapping("/testMVC")
+    @ResponseBody
+    public Map<String,Object> testMVC(){
+        Map<String,Object> responseMap = new HashMap<>();
+        responseMap.put("name","martin");
+        return responseMap;
+    }
+}
